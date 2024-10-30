c16d80bc8a1734b96e02a5dcb9261099ba8c9cf4
Simone Taeggi <startae14@gmail.com>
2024-10-30 11:01:01 +0100

    add  more testing

--- examples/basic-math/src/arithmetic.test.js
+++ examples/basic-math/src/arithmetic.test.js
@@ -5,6 +5,9 @@ describe('add', () => {
   it('should add two positive numbers', () => {
     expect(add(2, 2)).toBe(4);
   });
+  it('should add two negative numbers', () => {
+    expect(add(-2, -2)).toBe(-4);
+  });
 });
 
 describe('subtract', () => {
