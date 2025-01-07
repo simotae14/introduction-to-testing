95307c3bb223bbfb29a025a0efd9395b186ad131
Simone Taeggi <startae14@gmail.com>
2025-01-08 00:04:05 +0100

    Mocking Environment Variables

--- examples/logjam/src/log.test.js
+++ examples/logjam/src/log.test.js
@@ -16,3 +16,49 @@ describe('logger', () => {
     logSpy.mockRestore();
   });
 });
+
+describe('development', () => {
+  beforeEach(() => {
+    // stub the Mode dev
+    vi.stubEnv('MODE', 'development');
+  });
+
+  afterEach(() => {
+    // restore the original value of the MODE environment variable
+    vi.restoreAllMocks();
+  });
+
+  it('logs to the console in development mode', () => {
+    // Spy on console.log
+    const logSpy = vi.spyOn(console, 'log');
+
+    // Call the log function
+    log('Hello, world!');
+
+    // Verify the spy was called with the correct argument
+    expect(logSpy).toHaveBeenCalledWith('Hello, world!');
+  });
+});
+
+describe('production', () => {
+  beforeEach(() => {
+    // stub the Mode prod
+    vi.stubEnv('MODE', 'production');
+  });
+
+  afterEach(() => {
+    // restore the original value of the MODE environment variable
+    vi.restoreAllMocks();
+  });
+
+  it('does not log to the console in production mode', () => {
+    // Spy on console.log
+    const logSpy = vi.spyOn(console, 'log');
+
+    // Call the log function
+    log('Hello, world!');
+
+    // Verify the spy was called with the correct argument
+    expect(logSpy).not.toHaveBeenCalledWith('Hello, world!');
+  });
+});
