7cc865f278fa3409d2cb2d1e9e475ad622caef93
Simone Taeggi <startae14@gmail.com>
2025-01-07 15:25:53 +0100

    Example using Spy

--- examples/scratchpad/index.test.js
+++ examples/scratchpad/index.test.js
@@ -1,4 +1,4 @@
-import { test, expect } from 'vitest';
+import { test, expect, vi } from 'vitest';
 
 test('a super simple test', () => {
   expect(true).toBe(true);
@@ -8,4 +8,14 @@ test('a super simple test passing even if it is a wrong assertion', () => {
   setTimeout(() => {
     expect(true).toBe(false);
   }, 1000);
+});
+
+const logSpy = vi.spyOn(console, 'log');
+
+test('a super simple test with a spy', () => {
+  console.log('Hello, World!');
+  expect(logSpy).toBeCalled();
+  expect(logSpy).toBeCalledWith('Hello, World!');
+  expect(logSpy).toHaveBeenCalledOnce();
+  expect(logSpy).toHaveBeenCalledTimes(1);
 });
