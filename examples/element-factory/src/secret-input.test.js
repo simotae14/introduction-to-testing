1c474548b0034a96e1b8777bbb624c6286285235
Simone Taeggi <startae14@gmail.com>
2025-01-07 11:10:18 +0100

    Testing Local Storage

--- examples/element-factory/src/secret-input.test.js
+++ examples/element-factory/src/secret-input.test.js
@@ -4,10 +4,25 @@ import '@testing-library/jest-dom/vitest';
 
 import { createSecretInput } from './secret-input.js';
 
-describe.todo('createSecretInput', async () => {
-  beforeEach(() => {});
+describe('createSecretInput', async () => {
+  beforeEach(() => {
+    localStorage.clear();
+  });
 
   afterEach(() => {});
+
+  it('should store the value in localStorage', async () => {
+    const secretInput = createSecretInput();
+
+    const input = secretInput.querySelector('input');
+    const button = secretInput.querySelector('button');
+
+    input.value = 'my secret';
+    button.click();
+
+    // check the localStorage value
+    expect(localStorage.getItem('secret')).toBe('my secret');
+  });
 
   it('should have loaded the secret from localStorage', async () => {});
 
