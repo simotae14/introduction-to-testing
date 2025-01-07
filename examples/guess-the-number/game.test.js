f503094d3a0a35958779ed605f87b1f3abbe99d3
Simone Taeggi <startae14@gmail.com>
2025-01-07 23:57:28 +0100

    Mock Math random in guessing game

--- examples/guess-the-number/game.test.js
+++ examples/guess-the-number/game.test.js
@@ -2,6 +2,13 @@ import { it, expect, describe, vi } from 'vitest';
 import { Game } from './game';
 
 describe('Game', () => {
+  beforeEach(() => {
+    vi.spyOn(Math, 'random').mockReturnValue(0.5);
+  });
+
+  afterEach(() => {
+    vi.restoreAllMocks();
+  });
   it('should return an instance of a game', () => {
     // This is mostly a dummy test.
     const game = new Game();
@@ -14,5 +21,37 @@ describe('Game', () => {
     // Do I *really* care about the name of a "private" property?
     const game = new Game();
     expect(game.secretNumber).toBeTypeOf('number');
+    expect(game.secretNumber).toBe(50);
+  });
+
+  it('should return the correct response if the number is too low', () => {
+    const game = new Game();
+    expect(game.guess(49)).toBe('Too low!');
+  });
+
+  it('should return the correct response if the number is too high', () => {
+    const game = new Game();
+    expect(game.guess(51)).toBe('Too high!');
+  });
+
+  it('should return the correct response if the number is correct', () => {
+    const game = new Game();
+    expect(game.guess(50)).toBe(
+      'Correct! You guessed the number in 1 attempts.',
+    );
+  });
+
+  it('should return the correct response if the number is already guessed', () => {
+    const game = new Game();
+    game.guess(49);
+    expect(game.guess(49)).toBe('You already guessed that number!');
+  });
+
+  it('should return the correct number of guesses made', () => {
+    const game = new Game();
+    game.guess(49);
+    game.guess(51);
+    game.guess(50);
+    expect(game.guesses.size).toBe(3);
   });
 });
