8db028bc5beaae008a58fb35e5bba80ff50cda9c
Simone Taeggi <startae14@gmail.com>
2025-01-08 01:04:15 +0100

    Mocking the import of a dependency

--- examples/logjam/src/log.test.js
+++ examples/logjam/src/log.test.js
@@ -1,6 +1,12 @@
 import { expect, it, vi, beforeEach, afterEach, describe } from 'vitest';
 import { log } from './log';
 
+vi.mock('./send-to-server', () => {
+  return {
+    sendToServer: vi.fn(),
+  };
+});
+
 describe('logger', () => {
   it('logs to the console in development mode', () => {
     // Spy on console.log
@@ -15,50 +21,51 @@ describe('logger', () => {
     // Restore the original console.log function
     logSpy.mockRestore();
   });
-});
 
-describe('development', () => {
-  beforeEach(() => {
-    // stub the Mode dev
-    vi.stubEnv('MODE', 'development');
+  describe('development', () => {
+    beforeEach(() => {
+      // stub the Mode dev
+      vi.stubEnv('MODE', 'development');
+    });
+
+    afterEach(() => {
+      // restore the original value of the MODE environment variable
+      vi.restoreAllMocks();
+    });
+
+    it('logs to the console in development mode', () => {
+      // Spy on console.log
+      const logSpy = vi.spyOn(console, 'log');
+
+      // Call the log function
+      log('Hello, world!');
+
+      // Verify the spy was called with the correct argument
+      expect(logSpy).toHaveBeenCalledWith('Hello, world!');
+    });
   });
 
-  afterEach(() => {
-    // restore the original value of the MODE environment variable
-    vi.restoreAllMocks();
-  });
+  describe('production', () => {
+    beforeEach(() => {
+      // stub the Mode prod
+      vi.stubEnv('MODE', 'production');
+    });
 
-  it('logs to the console in development mode', () => {
-    // Spy on console.log
-    const logSpy = vi.spyOn(console, 'log');
+    afterEach(() => {
+      // restore the original value of the MODE environment variable
+      vi.restoreAllMocks();
+    });
 
-    // Call the log function
-    log('Hello, world!');
+    it('does not log to the console in production mode', () => {
+      // Spy on console.log
+      const logSpy = vi.spyOn(console, 'log');
 
-    // Verify the spy was called with the correct argument
-    expect(logSpy).toHaveBeenCalledWith('Hello, world!');
-  });
-});
+      // Call the log function
+      log('Hello, world!');
 
-describe('production', () => {
-  beforeEach(() => {
-    // stub the Mode prod
-    vi.stubEnv('MODE', 'production');
-  });
+      expect(logSpy).not.toHaveBeenCalled();
 
-  afterEach(() => {
-    // restore the original value of the MODE environment variable
-    vi.restoreAllMocks();
-  });
-
-  it('does not log to the console in production mode', () => {
-    // Spy on console.log
-    const logSpy = vi.spyOn(console, 'log');
-
-    // Call the log function
-    log('Hello, world!');
-
-    // Verify the spy was called with the correct argument
-    expect(logSpy).not.toHaveBeenCalledWith('Hello, world!');
+      expect(sendMock.sendToServer).toHaveBeenCalled();
+    });
   });
 });
