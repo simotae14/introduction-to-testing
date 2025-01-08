b6c8897e62bcd1210c445862ad7f89178d0702f6
Simone Taeggi <startae14@gmail.com>
2025-01-08 21:38:51 +0100

    Passing mocked args

--- examples/logjam/src/log.test.js
+++ examples/logjam/src/log.test.js
@@ -1,59 +1,37 @@
 import { expect, it, vi, beforeEach, afterEach, describe } from 'vitest';
 import { log } from './log';
 
-vi.mock('./send-to-server', () => {
-  return {
-    sendToServer: vi.fn(),
-  };
-});
-
-import { sendToServer } from './send-to-server';
-
 describe('logger', () => {
   describe('development', () => {
-    beforeEach(() => {
-      // stub the Mode dev
-      vi.stubEnv('MODE', 'development');
-    });
-
-    afterEach(() => {
-      // restore the original value of the MODE environment variable
-      vi.restoreAllMocks();
-    });
-
     it('logs to the console in development mode', () => {
-      // Spy on console.log
       const logSpy = vi.spyOn(console, 'log');
 
-      // Call the log function
-      log('Hello, world!');
+      log('Hello World');
 
-      // Verify the spy was called with the correct argument
-      expect(logSpy).toHaveBeenCalledWith('Hello, world!');
+      expect(logSpy).toHaveBeenCalledWith('Hello World');
+
+      vi.restoreAllMocks();
     });
   });
 
   describe('production', () => {
     beforeEach(() => {
-      // stub the Mode prod
       vi.stubEnv('MODE', 'production');
     });
 
     afterEach(() => {
-      // restore the original value of the MODE environment variable
       vi.restoreAllMocks();
     });
 
-    it('does not log to the console in production mode', () => {
-      // Spy on console.log
+    it('should not call console.log in production', () => {
       const logSpy = vi.spyOn(console, 'log');
-
-      // Call the log function
-      log('Hello, world!');
-
+      const sendToServerSpy = vi.fn();
+      log('Hello World', {
+        mode: 'production',
+        productionCallback: sendToServerSpy,
+      });
       expect(logSpy).not.toHaveBeenCalled();
-
-      expect(sendToServer).toHaveBeenCalled();
+      expect(sendToServerSpy).toHaveBeenCalled();
     });
   });
 });
