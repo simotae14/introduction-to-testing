db4580263960a3d74b77406620008acd271748ce
Simone Taeggi <startae14@gmail.com>
2025-02-09 18:07:26 +0100

    Inline Snapshots

--- examples/scratchpad/formatter.test.js
+++ examples/scratchpad/formatter.test.js
@@ -4,5 +4,5 @@ import { formatUser } from './formatter';
 test('formats user information correctly', () => {
   const user = { name: 'Alice', age: 30 };
   const formattedUser = formatUser(user);
-  expect(formattedUser).toMatchSnapshot();
+  expect(formattedUser).toMatchInlineSnapshot(`"User: Alice, Age: 30"`);
 });
