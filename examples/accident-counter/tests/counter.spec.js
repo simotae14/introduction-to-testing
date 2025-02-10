825f034bcf286d2f69787f9b7e670e6c6ea25f6b
Simone Taeggi <startae14@gmail.com>
2025-02-10 23:33:40 +0100

    Playwright test example on counter

---
+++ examples/accident-counter/tests/counter.spec.js
@@ -0,0 +1,13 @@
+import { test, expect } from '@playwright/test';
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('http://localhost:5173');
+});
+
+test('it has a counter', async ({ page }) => {
+  const count = page.getByTestId('counter-count');
+  const incrementButton = page.getByRole('button', { name: /increment/i });
+
+  await incrementButton.click();
+  expect(await count.textContent()).toBe('1');
+});
